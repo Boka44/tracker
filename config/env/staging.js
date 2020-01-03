8a6c524f7c3469a6898590a01f44860b0d9eb8f5
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-01-02 23:13:31 -0800

    Update staging.js

--- config/env/staging.js
+++ config/env/staging.js
@@ -1,10 +1,10 @@
 module.exports = {
     APP_NAME: 'agghog',
     mongoDb: {
-        db: 'agghog-uat',
-        connectionString: 'ds115071.mlab.com:15071',
-        user: 'agghog',
-        password: 's3cret'
+        db: '',
+        connectionString: '',
+        user: '',
+        password: ''
     },
     client: 'http://localhost:3000/',
     sessionTokenExpiry: 604800 // 7 days * 24 hours * 60 mins * 60 secs.,
