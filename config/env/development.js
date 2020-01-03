5464318ad7d4b57bcc49910d296ba66248a354ea
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-01-02 23:14:00 -0800

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,10 +1,10 @@
 module.exports = {
     APP_NAME: 'Tracker',
     mongoDb: {
-        db: 'gh-lite-dev',
-        connectionString: 'ds157735.mlab.com:57735',
-        user: 'gh',
-        password: 's3cret'
+        db: '',
+        connectionString: '',
+        user: '',
+        password: ''
     },
     client: 'http://localhost:3000/',
     sessionTokenExpiry: 604800 // 7 days * 24 hours * 60 mins * 60 secs.,
